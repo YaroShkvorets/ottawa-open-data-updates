18caa2caf63f26e2b9addb7389e0f631151a74b1
zzptichka <shkvorets@gmail.com>
2018-07-18 00:45:40 -0400

    added email notify from burner acct

--- fetch.js
+++ fetch.js
@@ -1,14 +1,48 @@
 const got = require('got');
-const today = new Date().toISOString().slice(0,10))
-//const today = "2018-07-17"
+const nodemailer = require('nodemailer');
+
+//const today = new Date().toISOString().slice(0,10)
+const today = "2018-07-17"
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ottopendata@gmail.com',
+    pass: 'opendata'
+  }
+});
+
+
+
+function sendEmail(body)
+{
+  var mailOptions = {
+    from: 'ottopendata@gmail.com',
+    to: 'shkvorets@gmail.com',
+    subject: 'New Ottawa Open Data update',
+    text: body
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
 
 got('http://data.ottawa.ca/api/3/action/recently_changed_packages_activity_list', { json: false }).then(response => {
   const res = JSON.parse(response.body)
+  let body = 'Found new objects: \n'
   for(let obj of res.result){
     if(obj.timestamp.indexOf(today)==0){
-      console.log(obj.timestamp)
+      body+=obj.timestamp+'\n'
+
     }
   }
+  sendEmail(body)
 
 }).catch(error => {
   console.log(error.response.body);
